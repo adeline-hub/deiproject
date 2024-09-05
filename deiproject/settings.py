dd23ba85087f848c51b3bbe00138aefb42315939
NamboLine <adeline@soulvest.io>
2024-09-05 12:02:42 +0200

    pushing to render

--- deiproject/settings.py
+++ deiproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +75,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'deiproject.wsgi.application'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 """
-import dj_database_url
 
 # Using dj_database_url to configure the database
 DATABASES = {
